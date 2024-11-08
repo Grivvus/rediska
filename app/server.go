d48099fe0d9c3e4ee99cf7b51c6e2e1736eaa66a
Arseniy <kruchik.a04@gmail.com>
2024-11-08 11:14:55 +0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"net"
 	"os"
+    "strings"
 )
+
+var storage map[string]string = make(map[string]string)
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -16,33 +19,59 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-    for {
-        connection, err := listner.Accept()
-        if err != nil {
-            fmt.Println("Error accepting connection: ", err.Error())
-            os.Exit(1)
-        }
-        go handleConnection(connection)
-    }
+	for {
+		connection, err := listner.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(connection)
+	}
 }
 
 func handleConnection(connection net.Conn) {
 	defer connection.Close()
-	read_buffer := make([]byte, 100)
-    for {
-        n, err := connection.Read(read_buffer)
-        if n == 0 {
-            break
-        }
-        if err != nil {
-            fmt.Println("Error accepting connection: ", err.Error())
-            os.Exit(1)
-        }
-        fmt.Printf("%v bytes recieved\n", n)
-        if string(read_buffer[:n]) == "*1\r\n$4\r\nPING\r\n" {
-            connection.Write([]byte("+PONG\r\n"))
-        } else if string(read_buffer[4:14]) == "$4\r\nECHO\r\n"{
-            connection.Write(read_buffer[14:n])
+	readBuffer := make([]byte, 100)
+	for {
+		n, err := connection.Read(readBuffer)
+		if n == 0 {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		fmt.Printf("%v bytes recieved\n", n)
+        parsedData := parse(readBuffer)
+		if parsedData[0] == "PING"{
+			connection.Write([]byte("+PONG\r\n"))
+		} else if parsedData[0] == "ECHO"{
+			connection.Write(readBuffer[14:n])
+		} else if parsedData[0] == "SET"{
+            if len(parsedData) != 3{
+                fmt.Fprintf(os.Stderr,"[Error] unexpected lenght of parsedData while setting data; expected 3, got %v", len(parsedData))
+                os.Exit(1)
+            }
+            storage[parsedData[1]] = parsedData[2]
+            connection.Write([]byte("+OK\r\n"))
+		} else if parsedData[0] == "GET"{
+            // if len(parsedData) != 2{
+            //     fmt.Fprintf(os.Stderr,"[Error] unexpected lenght of parsedData while getting data; expected 2, got %v", len(parsedData))
+            //     os.Exit(1)
+            // }
+            retStr := fmt.Sprintf("+%v\r\n", storage[parsedData[1]])
+            connection.Write([]byte(retStr))
+		}
+	}
+}
+
+func parse(buffer []byte) []string {
+    splited := strings.Split(string(buffer), "\r\n") 
+    var ret []string
+    for i := 1; i < len(splited); i++{
+        if i % 2 == 0 {
+            ret = append(ret, splited[i])
         }
     }
+    return ret
 }
