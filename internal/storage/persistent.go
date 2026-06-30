9d3b7d652f7ce3e70faa50a0201d40253d6cd637
Arseniy Krupchik <kruchik.a04@gmail.com>
2026-06-30 11:54:29 +0400

    add prototype of the rdb file parser

--- internal/storage/persistent.go
+++ internal/storage/persistent.go
@@ -1,8 +1,11 @@
 package storage
 
 import (
+	"encoding/binary"
 	"fmt"
 	"hash/crc64"
+	"io"
+	"slices"
 	"strconv"
 	"strings"
 	"time"
@@ -42,7 +45,8 @@ func EncodeToRDB(st *Storage) []byte {
 	_, _ = sb.Write([]byte{'F', 'F'})
 
 	crc := crc64.Checksum([]byte(sb.String()), nil)
-	_, _ = fmt.Fprintf(&sb, "%v", crc)
+	crcBin := binary.LittleEndian.AppendUint64(nil, crc)
+	_, _ = sb.Write(crcBin)
 	return []byte(sb.String())
 }
 
@@ -52,7 +56,7 @@ func encodeAuxilaryField(_ *Storage) []byte {
 
 func encodeDBSelector(_ *Storage) []byte {
 	const defaultDBSelector = "00"
-	return []byte("FE" + " " + defaultDBSelector)
+	return []byte("FE" + defaultDBSelector)
 }
 
 func encodeValues(st *Storage) []byte {
@@ -76,14 +80,130 @@ func encodeValues(st *Storage) []byte {
 func encodeStringValue(key, value string, timestamp *time.Time) []byte {
 	enc := make([]byte, 0, len(key)+len(value)+32)
 	if timestamp != nil {
-		enc = append(enc, []byte("FC ")...)
+		enc = append(enc, 'F', 'C')
 		enc = append(enc, []byte(strconv.FormatInt(timestamp.UnixMilli(), 10))...)
-		enc = append(enc, '\n')
 	}
-	enc = append(enc, byte(String), '\n')
+	enc = append(enc, byte(String))
 	enc = append(enc, codec.EncodeString(key)...)
-	enc = append(enc, '\n')
 	enc = append(enc, codec.EncodeString(value)...)
-	enc = append(enc, '\n')
 	return enc
+}
+
+type rdbStructure struct {
+	magic            [5]byte
+	rdbVersion       [4]byte
+	aux              auxilaryField
+	databaseSelector int
+	resizeHint       resizeHint
+	values           map[string]string
+	timestamps       map[string]time.Time
+	crcSum           [8]byte
+}
+
+type auxilaryField struct {
+	redisVersion string
+	creationTime time.Time
+}
+
+type resizeHint struct {
+	valueTableSize     int
+	timestampTableSize int
+}
+
+func DecodeRDB(r io.Reader) (rdbStructure, error) {
+	raw, err := io.ReadAll(r)
+	if err != nil {
+		return rdbStructure{}, fmt.Errorf("can't read rdb file: %w", err)
+	}
+	if !slices.Equal(raw[0:5], []byte(magicString)) {
+		return rdbStructure{}, fmt.Errorf("missing magic bytes at the start of the rdb file")
+	}
+	n := len(raw)
+	crcBin := raw[n-8 : n]
+	crc := binary.LittleEndian.Uint64(crcBin)
+	crcCalculated := crc64.Checksum(raw[0:n-8], nil)
+	if crc != crcCalculated {
+		return rdbStructure{}, fmt.Errorf("crc64 sum didn't match, rdb file may be corrupted")
+	}
+	i := 0
+	for i < len(raw)-1 && raw[i] != 'F' && raw[i+1] != 'E' {
+		i++
+	}
+	if raw[i] != 'F' && raw[i+1] != 'E' {
+		return rdbStructure{}, fmt.Errorf("can't find database selector block in rdb file")
+	}
+	rdb := rdbStructure{}
+	raw = raw[i+2:]
+	if raw[i] == 'F' && raw[i+1] == 'B' {
+		// parse resizedb fields
+	}
+	for len(raw) > 0 && (raw[0] != 'F' && raw[1] != 'F') {
+		shift, key, value, timestamp, err := parseKeyValuePair(raw)
+		if err != nil {
+			return rdb, err
+		}
+		rdb.values[key] = value
+		if timestamp != nil {
+			rdb.timestamps[key] = *timestamp
+		}
+		raw = raw[shift:]
+	}
+
+	return rdb, nil
+}
+
+func parseKeyValuePair(
+	encoded []byte,
+) (i int, key string, val string, timestamp *time.Time, err error) {
+	if encoded[i] == 'F' {
+		switch encoded[i+1] {
+		case 'C':
+			// ms timestamp is 8 byte long
+			unixMSBytes := encoded[i+2 : i+2+8]
+			unixMS, err := strconv.ParseInt(string(unixMSBytes), 10, 64)
+			if err != nil {
+				return 0, "", "", nil, fmt.Errorf("can't parse expiry timestamp: %w", err)
+			}
+			ts := time.UnixMilli(unixMS)
+			timestamp = &ts
+
+			// FC + 8 bytes timestamp
+			i += 10
+
+		case 'D':
+			// sec timestamp is 4 byte long
+			unixSecBytes := encoded[i+2 : i+2+4]
+			unixSec, err := strconv.ParseInt(string(unixSecBytes), 10, 32)
+			if err != nil {
+				return 0, "", "", nil, fmt.Errorf("can't prase expiry timestamp: %w", err)
+			}
+			ts := time.Unix(unixSec, 0)
+			timestamp = &ts
+
+			// FD + 4 bytes timestamp
+			i += 6
+		default:
+			return 0, "", "", nil, fmt.Errorf("invalid parsing state at %v", i+1)
+		}
+	}
+	valueType := RDBValueType(encoded[i])
+	i++
+	if valueType != String {
+		return 0, "", "", nil, fmt.Errorf("unsupported value type: %v", valueType)
+	}
+	key, err = codec.DecodeString(encoded[i:])
+	if err != nil {
+		return 0, "", "", nil, fmt.Errorf("can't decode key: %w", err)
+	}
+	// skip '$' of the key
+	i++
+	for i < len(encoded) && encoded[i] != '$' {
+		i++
+	}
+	val, err = codec.DecodeString(encoded[i:])
+	if err != nil {
+		return 0, "", "", nil, fmt.Errorf("can't decode value: %w", err)
+	}
+
+	return
 }
